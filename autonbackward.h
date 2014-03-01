5d777a03c4f016ca12597d544317819e7207df1d
SergeantFTC <noah@moeftc.org>
2014-03-01 14:16:31 -0500

    added IR scoring functions to new headers

--- autonbackward.h
+++ autonbackward.h
@@ -1,7 +1,28 @@
+#pragma systemfile
 
-task main()
-{
+void IRScoreBackward(){
+	clearEncoders();
+	wait1Msec(50);
+	const int totalTics = 7600;//total tics from before IR to end-- DONT CHANGE!
 
+	while(HTIRS2readACDir(IR) > 6){ //finds the beacon
+		nxtDisplayCenteredTextLine(1,"Direction: %d", HTIRS2readACDir(IR));
 
+		nxtDisplayCenteredTextLine(6,"%d",nMotorEncoder[leftDrive]);
+		//if(abs(nMotorEncoder[leftDrive]) >= totalTics-500) break;
+		startBackward(35);
+	}
+	stopDrive();
+	wait1Msec(300);
+	while(HTIRS2readACDir (IR)!= 5){
+		startBackward(15);
+	}
+	moveBackwardInchesNoReset(30, 8.75);//reverse back a small amount to correct for IR inaccuracy
 
+	stopDrive();//stops robot
+	servo[dumper] = 30;//dumps the block
+	motor[lift]= 50;//starts the lift up
+	wait1Msec(700);
+	motor[lift]= 0;//stops lift
+	servo[dumper] = servoRestPosition;//resets servo
 }
--- autonforward.h
+++ autonforward.h
@@ -1,7 +1,36 @@
+#pragma systemfile
 
-task main()
-{
+void scoreIRForward(){
+	moveForwardInches(50, 1); //away from wall
+	turn(g_PidTurn, 45); //turn to parallel with buckets
+	clearEncoders(); //clears encoder for the next step
 
+	const int totalTics = 7300; //6840 total tics from before IR to end-- DONT CHANGE!
+	int ticsToIR;
+	while(HTIRS2readACDir(IR) != 4){ //finds the beacon
+		nxtDisplayCenteredTextLine(5,"Direction:%d",HTIRS2readACDir(IR));
+		if(nMotorEncoder[leftDrive] >= totalTics-2000) break;
+		startForward(27);
+		ticsToIR = nMotorEncoder[leftDrive];
+		nxtDisplayCenteredTextLine(6,"%d",ticsToIR);
+	}
+	//stopDrive();
+	//PlaySound(soundBeepBeep);
+	stopDrive();
+	wait1Msec(300);
+	while(HTIRS2readACDir (IR) != 5){ //slow down to look for basket
+		startForward(15);
+	}
+	//while(true){}
+	stopDrive();//stops robot
 
+	//if(ticsToIR <= 3700){
+	moveForwardInchesNoReset(30,5);
+	//}
 
+	servo[dumper] = 30;//dumps the block
+	motor[lift]= 50;//starts the lift up
+	wait1Msec(700);
+	motor[lift]= 0;//stops lift
+	servo[dumper] = servoRestPosition;//resets servo
 }
